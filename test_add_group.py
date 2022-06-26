a8b905e7d22e1006fe2ab7a472a5dc91b8d3c1be
Anton M <34603432+RedBass909@users.noreply.github.com>
2022-06-26 13:12:50 +0300

    Улучшение читаемости тестов из первого видео второго урока

--- test_add_group.py
+++ test_add_group.py
@@ -16,20 +16,14 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="Test Group", header="Test Header", footer="Test Footer"))
-        self.rertur_to_group_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.rertur_to_group_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
@@ -39,6 +33,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -53,12 +48,14 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys("%s" % group.footer)
         # submit form creation
         wd.find_element_by_name("submit").click()
+        self.rertur_to_group_page(wd)
 
     def open_groups_page(self, wd):
         wd.find_element_by_id("content").click()
         wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("%s" % username)
         wd.find_element_by_name("pass").clear()
